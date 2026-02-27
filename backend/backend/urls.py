2b0812c095fc867a1032ee58524adc0288e93f92
Sanzhar Kasymzhomart <sanzhar@Mac.bbrouter>
2026-02-27 17:16:49 +0500

    f

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -8,6 +8,6 @@ import os
 admin.site.site_header = 'Панель Swift Store V2.0'
 
 urlpatterns = [
-    path('secretadmin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
