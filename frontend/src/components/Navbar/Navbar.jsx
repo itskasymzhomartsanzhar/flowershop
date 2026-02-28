1016ea20bb6d255c909f768ad9e020a1874c31bf
Sanzhar Kasymzhomart <sanzhar@Mac.bbrouter>
2026-02-28 12:43:49 +0500

    f

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -16,14 +16,6 @@ const Navbar = ({ setSearchVisible }) => {
       </div>
 
       <div className="navbar-group">
-        <Link to="/cart" className="navbar-link">
-          <button className="navbar-button navbar-button--cart">
-            {itemsCount > 0 && <span className="navbar-badge">{itemsCount}</span>}
-            <svg width="22" height="22" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M1.08334 1.08337H5.41668L8.32001 15.5892C8.41907 16.088 8.69041 16.536 9.08652 16.8549C9.48262 17.1737 9.97827 17.3431 10.4867 17.3334H21.0167C21.5251 17.3431 22.0207 17.1737 22.4168 16.8549C22.8129 16.536 23.0843 16.088 23.1833 15.5892L24.9167 6.50004H6.50001M10.8333 22.75C10.8333 23.3483 10.3483 23.8334 9.75001 23.8334C9.1517 23.8334 8.66668 23.3483 8.66668 22.75C8.66668 22.1517 9.1517 21.6667 9.75001 21.6667C10.3483 21.6667 10.8333 22.1517 10.8333 22.75ZM22.75 22.75C22.75 23.3483 22.265 23.8334 21.6667 23.8334C21.0684 23.8334 20.5833 23.3483 20.5833 22.75C20.5833 22.1517 21.0684 21.6667 21.6667 21.6667C22.265 21.6667 22.75 22.1517 22.75 22.75Z" stroke="currentColor" strokeWidth="2.3" strokeLinecap="round" strokeLinejoin="round"/>
-            </svg>
-          </button>
-        </Link>
         <a href="https://t.me/swiftagencyru" target="_blank" rel="noopener noreferrer">
           <button className="navbar-button">
             <img src={buttonimg} alt="logo" className="navbar-icon" />
