de0c40b0f727b585ce4adb4728a69c44382f55f6
Sanzhar Kasymzhomart <sanzhar@Mac.bbrouter>
2026-03-05 22:47:20 +0500

    f

--- frontend/src/components/OrderModal/OrderModal.jsx
+++ frontend/src/components/OrderModal/OrderModal.jsx
@@ -258,35 +258,21 @@ useEffect(() => {
             </div>
           </div>
 
-          {!order.is_pickup && (
+          {!order.is_pickup && order.delivery_address && (
             <div className="order-modal__delivery-section">
               <h3 className="order-modal__section-title">Информация о доставке</h3>
 
               <div className="order-modal__info-card">
                 <div className="order-modal__info-icon">
-                  <svg width="20" height="20" viewBox="0 0 20 20" fill="none">
-                    <path d="M10 2C6.13 2 3 5.13 3 9c0 5.25 7 11 7 11s7-5.75 7-11c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" fill="currentColor"/>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+                    <path fillRule="evenodd" d="M4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999zm2.493 8.574a.5.5 0 0 1-.411.575c-.712.118-1.28.295-1.655.493a1.3 1.3 0 0 0-.37.265.3.3 0 0 0-.057.09V14l.002.008.016.033a.6.6 0 0 0 .145.15c.165.13.435.27.813.395.751.25 1.82.414 3.024.414s2.273-.163 3.024-.414c.378-.126.648-.265.813-.395a.6.6 0 0 0 .146-.15l.015-.033L12 14v-.004a.3.3 0 0 0-.057-.09 1.3 1.3 0 0 0-.37-.264c-.376-.198-.943-.375-1.655-.493a.5.5 0 1 1 .164-.986c.77.127 1.452.328 1.957.594C12.5 13 13 13.4 13 14c0 .426-.26.752-.544.977-.29.228-.68.413-1.116.558-.878.293-2.059.465-3.34.465s-2.462-.172-3.34-.465c-.436-.145-.826-.33-1.116-.558C3.26 14.752 3 14.426 3 14c0-.599.5-1 .961-1.243.505-.266 1.187-.467 1.957-.594a.5.5 0 0 1 .575.411"/>
                   </svg>
                 </div>
                 <div className="order-modal__info-content">
-                  <span className="order-modal__info-label">Пункт сборки заказа</span>
-                  <span className="order-modal__info-value">Москва, Тестовый адрес</span>
+                  <span className="order-modal__info-label">Пункт назначения</span>
+                  <span className="order-modal__info-value">{order.delivery_address}</span>
                 </div>
               </div>
-
-              {order.delivery_address && (
-                <div className="order-modal__info-card">
-                  <div className="order-modal__info-icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
-                      <path fillRule="evenodd" d="M4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999zm2.493 8.574a.5.5 0 0 1-.411.575c-.712.118-1.28.295-1.655.493a1.3 1.3 0 0 0-.37.265.3.3 0 0 0-.057.09V14l.002.008.016.033a.6.6 0 0 0 .145.15c.165.13.435.27.813.395.751.25 1.82.414 3.024.414s2.273-.163 3.024-.414c.378-.126.648-.265.813-.395a.6.6 0 0 0 .146-.15l.015-.033L12 14v-.004a.3.3 0 0 0-.057-.09 1.3 1.3 0 0 0-.37-.264c-.376-.198-.943-.375-1.655-.493a.5.5 0 1 1 .164-.986c.77.127 1.452.328 1.957.594C12.5 13 13 13.4 13 14c0 .426-.26.752-.544.977-.29.228-.68.413-1.116.558-.878.293-2.059.465-3.34.465s-2.462-.172-3.34-.465c-.436-.145-.826-.33-1.116-.558C3.26 14.752 3 14.426 3 14c0-.599.5-1 .961-1.243.505-.266 1.187-.467 1.957-.594a.5.5 0 0 1 .575.411"/>
-                    </svg>
-                  </div>
-                  <div className="order-modal__info-content">
-                    <span className="order-modal__info-label">Пункт назначения</span>
-                    <span className="order-modal__info-value">{order.delivery_address}</span>
-                  </div>
-                </div>
-              )}
             </div>
           )}
 
